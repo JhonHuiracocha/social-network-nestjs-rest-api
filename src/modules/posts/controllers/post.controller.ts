4f7121fbcd06ce3e0b76433f23a4fc44fe6aebe7
Jhon Huiracocha <jhonhuiracocha123456789@gmail.com>
2022-06-19 23:25:37 -0500

    Posts response updated

--- src/modules/posts/controllers/post.controller.ts
+++ src/modules/posts/controllers/post.controller.ts
@@ -13,34 +13,39 @@ import { PostService } from '../services/post.service';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { PostEntity } from '../entities/post.entity';
 import { UpdatePostDto } from '../dto/update-post.dto';
+import { ApiResponse } from 'src/core/interfaces/response.interface';
 
 @Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto): Observable<PostEntity> {
+  create(
+    @Body() createPostDto: CreatePostDto,
+  ): Observable<ApiResponse<PostEntity>> {
     return this.postService.createPost(createPostDto);
   }
 
   @Get()
   findPosts(
-    @Query('take') take: number,
-    @Query('skip') skip: number,
-  ): Observable<PostEntity[]> {
-    take = take > 20 ? 20 : take;
-    return this.postService.findPosts(take, skip);
+    @Query('page') page: number,
+    @Query('limit') limit: number,
+  ): Observable<ApiResponse<PostEntity>> {
+    limit = limit > 20 ? 20 : limit;
+    return this.postService.findPosts(page, limit);
   }
 
   @Get(':postId')
-  findPostById(@Param('postId') postId: string): Observable<PostEntity> {
+  findPostById(
+    @Param('postId') postId: string,
+  ): Observable<ApiResponse<PostEntity>> {
     return this.postService.findPostWithRelations(postId);
   }
 
   @Get('/author/:authorId')
   getAuthorPosts(
     @Param('authorId') authorId: string,
-  ): Observable<PostEntity[]> {
+  ): Observable<ApiResponse<PostEntity>> {
     return this.postService.getAuthorPosts(authorId);
   }
 
@@ -48,12 +53,14 @@ export class PostController {
   updatePost(
     @Param('postId') postId: string,
     @Body() updatePostDto: UpdatePostDto,
-  ): Observable<PostEntity> {
+  ): Observable<ApiResponse<PostEntity>> {
     return this.postService.updatePost(postId, updatePostDto);
   }
 
   @Delete(':postId')
-  deletePost(@Param('postId') postId: string): Observable<PostEntity> {
+  deletePost(
+    @Param('postId') postId: string,
+  ): Observable<ApiResponse<PostEntity>> {
     return this.postService.deletePost(postId);
   }
 }
--- src/modules/posts/services/post.service.ts
+++ src/modules/posts/services/post.service.ts
@@ -1,10 +1,11 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Observable, from, tap } from 'rxjs';
-import { Repository } from 'typeorm';
+import { Observable, from, tap, catchError, switchMap, of } from 'rxjs';
+import { Repository, UpdateResult } from 'typeorm';
 import { PostEntity } from '../entities/post.entity';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { UpdatePostDto } from '../dto/update-post.dto';
+import { ApiResponse } from '../../../core/interfaces/response.interface';
 
 @Injectable()
 export class PostService {
@@ -13,11 +14,26 @@ export class PostService {
     private readonly postRepository: Repository<PostEntity>,
   ) {}
 
-  createPost(createPostDto: CreatePostDto): Observable<PostEntity> {
-    return from(this.postRepository.save(createPostDto));
+  createPost(
+    createPostDto: CreatePostDto,
+  ): Observable<ApiResponse<PostEntity>> {
+    return from(this.postRepository.save(createPostDto)).pipe(
+      switchMap((post: PostEntity) => {
+        return of({
+          status: true,
+          message: 'The post has been created successfully.',
+          data: [post],
+        });
+      }),
+    );
   }
 
-  findPosts(take: number = 10, skip: number = 0): Observable<PostEntity[]> {
+  findPosts(
+    page: number = 1,
+    limit: number = 0,
+  ): Observable<ApiResponse<PostEntity>> {
+    const skip: number = (page - 1) * limit;
+
     return from(
       this.postRepository.find({
         where: {
@@ -29,13 +45,23 @@ export class PostService {
             author: true,
           },
         },
-        take,
+        take: limit,
         skip,
+      }),
+    ).pipe(
+      switchMap((posts: PostEntity[]) => {
+        return of({
+          status: true,
+          data: posts,
+          info: {
+            page,
+          },
+        });
       }),
     );
   }
 
-  findPostWithRelations(id: string): Observable<PostEntity> {
+  findPostWithRelations(id: string): Observable<ApiResponse<PostEntity>> {
     return from(
       this.postRepository.findOne({
         where: {
@@ -50,11 +76,20 @@ export class PostService {
         },
       }),
     ).pipe(
-      tap((post: PostEntity) => {
-        if (!post) {
-          throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
-        }
-        return post;
+      switchMap((post: PostEntity) => {
+        if (!post)
+          throw new HttpException(
+            {
+              status: false,
+              message: 'The post has not been found.',
+            },
+            HttpStatus.NOT_FOUND,
+          );
+
+        return of({
+          status: true,
+          data: [post],
+        });
       }),
     );
   }
@@ -67,17 +102,10 @@ export class PostService {
           status: true,
         },
       }),
-    ).pipe(
-      tap((post: PostEntity) => {
-        if (!post) {
-          throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
-        }
-        return post;
-      }),
     );
   }
 
-  getAuthorPosts(authorId: string): Observable<PostEntity[]> {
+  getAuthorPosts(authorId: string): Observable<ApiResponse<PostEntity>> {
     return from(
       this.postRepository.find({
         where: {
@@ -92,22 +120,58 @@ export class PostService {
           },
         },
       }),
-    );
-  }
-
-  updatePost(id: string, updatePostDto: UpdatePostDto): Observable<PostEntity> {
-    return this.findPostById(id).pipe(
-      tap((post: PostEntity) => {
-        updatePostDto.id = post.id;
-        return from(this.postRepository.save(updatePostDto));
+    ).pipe(
+      switchMap((posts: PostEntity[]) => {
+        return of({
+          status: true,
+          message: 'Find post @take & @skip.',
+          data: posts,
+        });
       }),
     );
   }
 
-  deletePost(id: string): Observable<PostEntity> {
+  updatePost(
+    id: string,
+    updatePostDto: UpdatePostDto,
+  ): Observable<ApiResponse<PostEntity>> {
     return this.findPostById(id).pipe(
-      tap((post: PostEntity) => {
-        return from(this.postRepository.save({ ...post, status: false }));
+      switchMap((post: PostEntity) => {
+        if (!post)
+          throw new HttpException(
+            { status: false, message: 'The post has not been found.' },
+            HttpStatus.NOT_FOUND,
+          );
+
+        return from(this.postRepository.update(id, updatePostDto)).pipe(
+          switchMap((updateResult: UpdateResult) => {
+            return of({
+              status: true,
+              message: 'The post has been updated successfully.',
+            });
+          }),
+        );
+      }),
+    );
+  }
+
+  deletePost(id: string): Observable<ApiResponse<PostEntity>> {
+    return this.findPostById(id).pipe(
+      switchMap((post: PostEntity) => {
+        if (!post)
+          throw new HttpException(
+            { status: false, message: 'The post has not been found.' },
+            HttpStatus.NOT_FOUND,
+          );
+
+        return from(this.postRepository.update(id, { status: false })).pipe(
+          switchMap((updateResult: UpdateResult) => {
+            return of({
+              status: true,
+              message: 'The post has been deleted successfully.',
+            });
+          }),
+        );
       }),
     );
   }
